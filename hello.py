acda98b2c3a3aa2b470a78d0195b4c355c01cd1f
EC2 Default User <ec2-user@ip-172-31-24-161.eu-north-1.compute.internal>
2023-06-02 11:34:40 +0000

    experimenting

--- hello.py
+++ hello.py
@@ -1,16 +1,15 @@
+from flask import Flask, url_for, request
 import os
-import pdb
-from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def say_hello():
-    # pdb.set_trace()
-    return "Hello Flask!"
+@app.route('/', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        return f'<h1>Welcome {request.values["user"]}!</h1>'
+    return '<form method="post" action="/"><input type="text" name="user"><p><input type="password"><p><input type="submit" value="Submit"></form>'
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
-    app.debug = True
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
